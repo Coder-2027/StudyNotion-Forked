1440d8811d04a07129f0014685dcf1c96b595eba
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-07-01 18:02:40 +0530

    Update SubSection.js

--- Server/Model/SubSection.js
+++ Server/Model/SubSection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const SubSectionSchema = new mongoose.Schema({
+*const SubSectionSchema = new mongoose.Schema({
   title: { type: String },
   timeDuration: { type: String },
   description: { type: String },
