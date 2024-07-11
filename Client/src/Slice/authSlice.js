482432cc79b6e9f8cf74f7ef90fbdabe472e5159
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-07-11 17:26:54 +0530

    Update authSlice.js

--- Client/src/Slice/authSlice.js
+++ Client/src/Slice/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  signupData: null,
+  signupData: null,                  //this will contain info of user
   loading: false,
   token: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null,            //trying to get data from local storage where data remains
   // stored even when we have shut our website
