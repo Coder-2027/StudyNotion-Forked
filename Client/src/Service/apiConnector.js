100af6bd821b53eb347fdace4635039d59abb4ed
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-07-11 17:38:19 +0530

    Update apiConnector.js

--- Client/src/Service/apiConnector.js
+++ Client/src/Service/apiConnector.js
@@ -11,3 +11,13 @@ export const apiConnector = (method, url, bodyData, headers, params) => {
     params: params ? params : null,
   });
 };
+
+
+// The apiConnector function takes several parameters:
+// method: The HTTP method (e.g., “GET,” “POST,” “PUT,” etc.).
+// url: The URL to which the request will be made.
+// bodyData: Optional data to send in the request body (used for POST or PUT requests).
+// headers: Optional headers to include in the request.
+// params: Optional query parameters to append to the URL.
+// Inside the function, it constructs an Axios request using the provided parameters.
+// The request is made using the axiosInstance created earlier.
